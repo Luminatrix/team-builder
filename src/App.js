4a8afb2174934461079f65dfc1c8c97b21036e7d
Stella <stella.kate.edelmann@gmail.com>
2019-11-12 16:37:48 -0800

    form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import TeamBuilder from "./Form"
+import Card from "./Card"
 
 function App() {
+  const [teamMember, setTeamMember] = useState([
+    {
+      Name: "Magical McMythicson",
+      Email: "UnicornDev@Team.com",
+      Role: "Head Unicorn Dev"
+    }
+  ])
+
+  const addNewMember = member => {
+    const newTeamMember = setTeamMember([...teamMember, member]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Mystical Team Members</h1>
+
+     <TeamBuilder addNewMember={addNewMember}/>
+     <Card teamMember={teamMember} />
+
     </div>
   );
 }
